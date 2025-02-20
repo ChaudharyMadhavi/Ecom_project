89081ed6eb008d13d57796a559b0bf5a41ceaacd
Madhavi chaudhary <132742045+ChaudharyMadhavi@users.noreply.github.com>
2025-02-20 07:21:16 +0530

    Delete backend/ecom-project directory

--- backend/ecom-project/src/main/java/com/example/ecom_project/Repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecom_project.Repository;
-
-import com.example.ecom_project.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product,Integer> {
-}
